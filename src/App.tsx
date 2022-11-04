17364c1cbb8153e05ef3667351e0359769ad7dce
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-11-04 17:03:56 -0400

    making home page look cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import StorePage from './pages/StorePage';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="container mx-auto">
+      <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/shop" element={<StorePage />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<StorePage />} />
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   )
 }
 
