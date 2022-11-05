237d381f6d3c9ffc120852b9f0b3394022b41535
bluephosphor <66392799+bluephosphor@users.noreply.github.com>
2022-11-04 23:45:49 -0400

    shopping cart......

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,29 @@ import Navbar from './components/Navbar';
 import RegisterModal from './components/RegisterModal';
 import Home from './pages/Home';
 import StorePage from './pages/StorePage';
+import { Cart } from './models/cart';
+import { useState } from 'react';
+import { User } from './models/user';
 
 function App() {
-  return (
-    <div className="container mx-auto">
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/shop" element={<StorePage />}/>
-        <Route path="/items/:id" element={< SingleItem />}/>
-      </Routes>
-      <LoginModal />
-      <RegisterModal />
-      </BrowserRouter>
-    </div>
-  )
+
+    const [cart, setCart] = useState<Cart>(new Cart());
+    const [loggedInUser, setLoggedInUser] = useState<User>();
+
+    return (
+        <div className="container mx-auto">
+            <BrowserRouter>
+                <Navbar user={loggedInUser} cart={cart} setCart={setCart}/>
+                <Routes>
+                    <Route path="/" element={<Home cart={cart} setCart={setCart}/>} />
+                    <Route path="/shop" element={<StorePage cart={cart} setCart={setCart}/>} />
+                    <Route path="/items/:id" element={< SingleItem />} />
+                </Routes>
+                <LoginModal />
+                <RegisterModal />
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
